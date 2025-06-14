949feef8539fa83195ca5cc670fde666008fd86a
Ana Carolina <aanaakaarolina@gmail.com>
2025-06-14 19:19:40 -0300

    adição de novos atributos na classe Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,6 +2,12 @@
 public class Cliente {
 
 	private String nome;
+	private String dataNasc;
+	private String cpf;
+	private String telefone;
+	private String sexo;
+	private EstadoCivil estadoCivil;
+	private Endereco endereco;
 
 	public String getNome() {
 		return nome;
@@ -9,6 +15,54 @@ public class Cliente {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+		public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public EstadoCivil getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(EstadoCivil estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
 	}
 
 }
